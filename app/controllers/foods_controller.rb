33c6d96d7b42b7bb5d90fe390fa42d2db2c16d57
Karel van Oordt <vanoordtkarel@gmail.com>
2022-03-14 15:36:18 -0500

    Update food controller, model & view

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,26 @@
 class FoodsController < ApplicationController
+
+    def index
+    end
+
+    def create
+        @food = Food.new(food_params)
+        @food.user_id = current_user.id
+        if @food.save
+            flash[:success] = 'Food created succesfully'
+            redirect_to foods_path
+
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+    end
+
+    private
+
+    def food_params
+        params.require(:food).permit(:name, :measurement_unit, :price)
+    end
 end
