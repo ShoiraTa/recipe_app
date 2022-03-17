8ba331d6fb730c29ce927b451067f423c24c29a7
shoiraTa <shoira.shakirovna@gmail.com>
2022-03-17 09:56:06 +0400

    Fixbug add turbo false to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_up_path_for(_resource_or_scope)
+    new_user_session
+  end
 end
